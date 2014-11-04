2719f599e84a4119c2263498bce3f5ffba0cb371
Zhonghua Xi <xizhonghua@gmail.com>
2014-11-04 18:36:23 -0500

    update google-num

---
+++ google-num/google-num.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+import random
+
+with open('result.txt', 'a') as output:
+    for k in range(1,10000):
+        i = random.randint(0, 1000000000)
+        os.system('(echo "GET /search?q=' + str(i) + '" ; sleep 1) | telnet www.google.com 80 > tmp')
+        with open('tmp', 'r') as f:
+            data = f.read()
+        os.system('rm tmp; sleep 0.1')
+
+        s = data.find('>About')
+        t = data.find('results<', s)
+        text = data[s+7:t-1];
+        if len(text)<=0 or len(text)>=15: continue
+        result = int(text.replace(',',''))
+        output.write(str(i) + ' ' + str(result)+'\n')
+        output.flush()
+        
